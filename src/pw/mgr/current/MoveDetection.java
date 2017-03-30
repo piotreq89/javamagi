58145c5a0aac244b2760a606ff631eb38e7cb9bd
piotreq <pwalenda89@gmail.com>
2017-03-30 17:30:43 +0200

    Ujednolicenie wersji.

--- src/pw/mgr/current/MoveDetection.java
+++ src/pw/mgr/current/MoveDetection.java
@@ -4,6 +4,9 @@ import org.opencv.core.*;
 import org.opencv.core.Point;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.video.BackgroundSubtractor;
+import org.opencv.video.BackgroundSubtractorMOG2;
+import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
 
 import javax.imageio.ImageIO;
@@ -21,11 +24,6 @@ public class MoveDetection {
         System.loadLibrary("opencv_java310");
     }
 
-    private static int i = 0 ;
-    private static  int k = 0 ;
-    private static Mat previousFrame = null;
-
-
     private static List<Point> myPoints = new ArrayList<>();
     private static MyFrame myFrame = new MyFrame();
     private static Mat firstScreen = null;
@@ -41,7 +39,7 @@ public class MoveDetection {
         Mat frame = new Mat();
 
         // kalasa z funkcjami do obrobki video
-        VideoCapture video = new VideoCapture("D:\\moje\\magi\\test_film1.mp4");
+        VideoCapture video = new VideoCapture("C:\\Users\\piotrek\\Desktop\\test\\test_film1.mp4");
 
 
         Mat morphOutput = new Mat();
@@ -51,7 +49,7 @@ public class MoveDetection {
             public void run() {
                 System.out.println("show");
                 while (startClass.isStart()){
-                    showframe(previousFrame, morphOutput, frame, video);
+                    showframe(frame, video);
                 }
             }
         }
@@ -68,50 +66,104 @@ public class MoveDetection {
             startClass.setStart(false);
         });
 
-    myFrame.getDrawMoveButton().addActionListener(e -> {
-        System.out.println("print points");
-        MyRunPrint myRunPrint = new MyRunPrint(myPoints);
-        myRunPrint.start();
-        startClass.setStart(true);
-    });
+        myFrame.getDrawMoveButton().addActionListener(e -> {
+            System.out.println("print points");
+            MyRunPrint myRunPrint = new MyRunPrint(myPoints);
+            myRunPrint.start();
+            startClass.setStart(true);
+        });
     }
 
 
-    private static void showframe(Mat previousFrame,  Mat morphOutput, Mat frame, VideoCapture video) {
+    private static void showframe(Mat frame, VideoCapture video) {
 
-        Mat tempon_frame ;
-        Mat diff_frame;
+        /**pierwszy ekran*/
+
         Mat secondFrame;
-
         video.read(frame);
+//        Imgproc.resize(frame, frame,new Size(710, 400));
         Imgproc.resize(frame, frame,new Size(710, 400));
         firstScreen = frame;
+
+        /**drugi ekran*/
 
         secondFrame = new Mat(frame.size(), CvType.CV_8UC1);
         //Konwertuje obraz do skali szarości
         Imgproc.cvtColor(frame, secondFrame, Imgproc.COLOR_BGR2GRAY);
         //Rozmycie filtrem Gaussa, usuwa on detale i eliminuje zakłócenia
         // src, dst, wielkośc jądra gausa, odchylenie standardowe jądra w kierunku X
-        Imgproc.GaussianBlur(secondFrame, secondFrame, new Size(3, 3), 0);
+        Imgproc.GaussianBlur(secondFrame, secondFrame, new Size(3, 3), 1.0, 1.0, 0);
         secondScreen = secondFrame ;
 
-        if(MoveDetection.i == 0){
-            previousFrame = new Mat(frame.size(), CvType.CV_8UC1);
-            MoveDetection.i = 1 ;
-        }
+        /**trzeci ekran*/
 
-        diff_frame = new Mat(secondFrame.size(), CvType.CV_8UC1);
+        Mat subtractedFrame = new Mat(secondFrame.size(), CvType.CV_8UC1);
+        Mat fgmask =  new Mat(secondFrame.size(), CvType.CV_8UC1);
 
-        // Oblicza różnicę między dwiema macieżami
-        Core.subtract(previousFrame, secondFrame, diff_frame);
+        //BS oblicza maskę pierwszego planu, wykonując odejmowanie między bieżącą ramką a modelem tła,
+        // zawierającą statyczną część sceny lub ogólnie wszystko, co można uznać za tło,
+        // biorąc pod uwagę charakterystykę obserwowanej sceny
 
-        //Oblicza różnicę bezwzględną między poszczególnymi elementami tablicy
-//        Core.absdiff(previousFrame, secondFrame, diff_frame);
+        //Modelowanie tła składa się z dwóch głównych etapów: Inicjalizacja tła; Aktualizacja w tle.
+        // W pierwszym kroku obliczany jest początkowy model tła, podczas gdy w drugim etapie model
+        // jest aktualizowany w celu dostosowania się do możliwych zmian w scenie.
 
-//        Imgproc.Laplacian(previousFrame, previousFrame, previousFrame.depth());
-//        thirdScreen = diff_frame.clone();
+//        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3, 3));
+        //BackgroundSubtractor zostaną użyte do wygenerowania masek pierwszego planu.
+        // Zapewnia lepszą adaptowalność do zmieniających się scen dzięki zmianom oświetlenia itp.
+        BackgroundSubtractorMOG2 mog2 = Video.createBackgroundSubtractorMOG2();
+        //Oblicza maskę pierwszego planu
+        //Następna klatka wideo, Maska pierwszoplanowa wyjściowa jako 8-bitowy obraz binarny.,
+        //Każda ramka jest używana zarówno do obliczania nowej warstwy, jak i do uaktualniania tła.
+        System.out.println(
+                " \nmog2.getHistory() " + mog2.getHistory() +
+                        " \nmog2.getBackgroundRatio()  " + mog2.getBackgroundRatio() +
+                        " \nmog2.getDetectShadows() " + mog2.getDetectShadows() +
+                        " \nmog2.getVarThreshold() " + mog2.getVarThreshold() +
+                        "\n-------------------------------------------------"
+        );
+        mog2.apply(secondFrame, fgmask);
 
-        Mat finalFrame = diff_frame.clone();
+//        Imgproc.morphologyEx(fgmask, subtractedFrame, Imgproc.MORPH_OPEN, kernel);
+
+        subtractedFrame =fgmask;
+
+        Mat flow = new Mat(secondFrame.size(),CvType.CV_8UC1);
+//        Mat flow2 = new Mat(secondFrame.size(),CvType.CV_8UC1);
+//        Mat flow3 = new Mat(secondFrame.size(),CvType.CV_8UC1);
+//        Video.calcOpticalFlowFarneback(subtractedFrame, subtractedFrame, flow ,0.5,1, 1, 1, 7, 1.5,1);
+////
+//        flow2 = flow ;
+//        Core.normalize(flow, flow, 0, 255, Core.NORM_MINMAX);
+//        Imgproc.cvtColor(flow, flow, Imgproc.COLOR_BGR2GRAY);
+//        thirdScreen = flow2.clone();
+
+        thirdScreen = subtractedFrame;
+
+//        MatOfPoint corners  = new MatOfPoint();
+//        Imgproc.goodFeaturesToTrack(secondFrame,corners,300,0.01,10);
+//
+//        Mat gray = new Mat(secondFrame.size(),CvType.CV_8UC1);
+//        Imgproc.cvtColor(subtractedFrame,gray, Imgproc.COLOR_BGR2GRAY);
+//
+//        MatOfPoint2f mp2f = new MatOfPoint2f(corners.toArray());
+//        Imgproc.cornerSubPix(subtractedFrame, mp2f, new Size(10, 10), new Size(-1, -1), new TermCriteria());
+//
+//        List<Point> lp = mp2f.toList();
+//        MatOfPoint2f[] points = new MatOfPoint2f[4];
+//        points[1].fromList(lp);
+//
+//        MatOfByte status = new MatOfByte();
+//        MatOfFloat err = new MatOfFloat();
+//
+//        Video.calcOpticalFlowPyrLK(subtractedFrame,gray, points[0], points[1],  status, err);
+
+//        thirdScreen = gray.clone();
+//        points[0] = MatOfPoint2f.pointSwap(points[1], points[1] = points[0]);
+
+        /**czwarty ekran*/
+
+        Mat finalFrame = new Mat(secondFrame.size(), CvType.CV_8UC1);
 
 //        Imgproc.adaptiveThreshold(diff_frame, diff_frame, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 2);
         // Adaptacyjne progi, metoda oblicza wartości progowe, usuwa szumy, czyli filtruje piksele z za małą albo za dużą wartością
@@ -121,50 +173,11 @@ public class MoveDetection {
         // - ADAPTIVE_THRESH_GAUSSIAN_C - wartość progowa jest ważoną sumą wartości sąsiedztwa, gdzie wagi są oknami Gaussa. ,
         // wielkość pikseli sąsiedztwa używana do obliczania wartości progowej. ,
         // stałą używaną w obu metodach (odejmowana od średniej lub ważonej średniej).
-        Imgproc.adaptiveThreshold(diff_frame, finalFrame, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 5, 2);
+        Imgproc.adaptiveThreshold(subtractedFrame, finalFrame, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 5, 2);
 
-        thirdScreen = finalFrame.clone();
-
-        Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, 1));
-        Imgproc.erode(finalFrame, finalFrame, erodeElement);
-
-        Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, 1));
-        Imgproc.dilate(finalFrame, finalFrame, dilateElement);
-        Imgproc.dilate(finalFrame, finalFrame, dilateElement);
-        Imgproc.dilate(finalFrame, finalFrame, dilateElement);
-        Imgproc.dilate(finalFrame, finalFrame, dilateElement);
-
-
-//
         fourthScreen = finalFrame.clone();
 
-
-//
-//        Scalar minValues = new Scalar(0, 0, 0);
-//        Scalar maxValues = new Scalar(180, 255, 148);
-
-//        Imgproc.dilate(diff_frame, morphOutput, dilateElement);
-
-        // bierze tylko obiekty z tego zakresu kolorow
-//        Core.inRange(diff_frame, minValues, maxValues, mask);
-//        Imgproc.erode(diff_frame, morphOutput, erodeElement);
-////					Imgproc.erode(mask, morphOutput, erodeElement);
-//
-////					Imgproc.dilate(mask, morphOutput, dilateElement);
-//                    Imgproc.dilate(mask, morphOutput, dilateElement);
-//
-//                    Mat2bufferedImage(morphOutput);
-
-
-
         detection_contours(fourthScreen);
-
-        MoveDetection.k++ ;
-
-//        if(k % 10 == 0 || i == 0){
-            MoveDetection.previousFrame = secondFrame.clone();
-
-//        }
 
         putScreenInVideoLabel();
     }
@@ -187,6 +200,8 @@ public class MoveDetection {
             Iterator<Rect> it2 = array.iterator();
             while (it2.hasNext()) {
                 Rect obj = it2.next();
+                obj.height += 20;
+                obj.width +=10;
                 Imgproc.rectangle(firstScreen, obj.br(), obj.tl(), new Scalar(0, 255, 0), 1);
             }
         }
